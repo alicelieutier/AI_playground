6d54848ced48f01bdff9d96eeccfb7d28dad84bc
Alice Lieutier <alice@lieutier.me>
2020-06-06 20:20:26 +0100

    clean up tictactoe backend

--- back_end/tictactoe.py
+++ back_end/tictactoe.py
@@ -1,27 +1,13 @@
 import json
 
-class NoValidMove(Exception):
-    "Raised when there are no valide moves left in grid"
-
-    def __init__(self, message):
-        self.message = message
-
-
 def next_move(data):
   grid = data['grid']
   possible_cells = ((i, j) for i in range(3) for j in range(3))
   empty_cells = ((i, j) for (i, j) in possible_cells if len(grid[i][j]) == 0)
-  # return first empty cell
-  # possible_cells = ((i, j) for i in range(3) for j in range(3))
-  # p1 = {tuple(position) for position in data['moves']['p1']}
-  # p2 = {tuple(position) for position in data['moves']['p2']}
-  # cell = next(possible_cells)
   try:
     return {'play': next(empty_cells)}
   except StopIteration:
     return {'error': 'No empty space left in grid'}
-    # raise NoValidMove('No empty space left in grid')
-
 
 if __name__ == "__main__":
   test_data = {
@@ -36,7 +22,4 @@ if __name__ == "__main__":
   }
   # next_move(full_grid)  # <-- should raise
 
-# you = X
-#__0
-#X0_
-#X__
+
